bb34a99efce820fa8cdd45206e3ac323ba291ef2
Ali2174 <vanbasten100pes@gmail.com>
2024-02-07 15:00:01 +0400

    yoxlama

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,14 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class Main {
     public static void main(String[] args) {
         WebDriver driver=new ChromeDriver();
-        driver.get("https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=ASKXGp0lABS70lcqc-6ki1YizATh1sxFrAAEiNWTzUrYRlxuniaoo6LjsHYofHdPERbEb-fZgkvr0w&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-670920373%3A1707219054443971&theme=glif");
+        driver.get("https://qaplayground.dev/apps/covered/#");
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-popup-blocking");
-        WebElement mailInput=driver.findElement(By.id("identifierId"));
-        mailInput.sendKeys("vanbasten100pes@gmail.com");
-        WebElement nextBtn1=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[2]/div/div[1]/div/div/button"));
-nextBtn1.click();
 
-WebElement nextBtn2=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[2]/div/div[1]/div/div/button"));
+        WebElement mailInput=driver.findElement(By.id("info"));
+
+        WebElement scroll=driver.findElement(By.className("wrapper"));
+
 
 
     }
