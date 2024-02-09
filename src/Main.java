45e41c13382d3cc029cb5f0bfcd39735c38f3b0b
Ali2174 <vanbasten100pes@gmail.com>
2024-02-09 14:16:35 +0400

    Range Rate Slider

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,18 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 
 public class Main {
+    public static void asserts(String exp, String act){
+
+        if(exp.equals(act)){
+
+         System.out.println("true");
+     }else{
+         System.out.println("Problem");
+     }
+
+    }
     public static void main(String[] args) throws InterruptedException {
+
         WebDriver driver=new ChromeDriver();
         driver.get("https://qaplayground.dev/apps/range-slider/");
         ChromeOptions options = new ChromeOptions();
@@ -16,18 +27,24 @@ public class Main {
         WebElement slider=driver.findElement(By.xpath("/html/body/div/div[2]/input"));
         Thread.sleep(1000);
         Actions actions=new Actions(driver);
+        // Slide
         actions.dragAndDropBy(slider,-50,0).perform();
         Thread.sleep(1000);
         actions.dragAndDropBy(slider,0,0).perform();
      WebElement feedback= driver.findElement(By.id("feedback"));
         Thread.sleep(1000);
      feedback.click();
+
      WebElement tymsg=driver.findElement(By.id("ty-msg"));
-     if(tymsg.toString().equals("Thank you for your feedback!")){
-         System.out.println("true");
-     }else{
-         System.out.println("Problem");
-     }
-     driver.quit();
+        System.out.println(tymsg.getText());
+
+        asserts("Thank you for your feedback!",tymsg.getText());
+//     if(tymsg.getText().("Thank you for your feedback!")){
+//         System.out.println("true");
+//     }else{
+//         System.out.println("Problem");
+//     }
+     Thread.sleep(3000);
+driver.quit();
     }
 }
